45204828290a28d962bd531036e4aad8d935bc4f
Carlos Alonso Ferreira <carlos.alonsof@udc.es>
2023-12-03 01:42:09 +0100

    Day 2 refactor

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -4,22 +4,11 @@ defmodule AdventOfCode.Day02 do
     |> String.split("\n", trim: true)
     |> Enum.map(&String.split(&1, ":", trim: true))
     |> Enum.filter(fn [_, cubes] ->
-      tokens = String.split(cubes, ";", trim: true)
-
-      filtered =
-        Enum.filter(tokens, fn token ->
-          count_color(token, "red") <= 12 and count_color(token, "green") <= 13 and
-            count_color(token, "blue") <= 14
-        end)
-
-      length(filtered) == length(tokens)
+      cubes
+      |> String.split(";", trim: true)
+      |> Enum.all?(&valid_pull?/1)
     end)
-    |> Enum.map(fn [game, _] ->
-      case Regex.run(~r/Game (\d+)/, game) do
-        [_, game_number] -> String.to_integer(game_number)
-        _ -> nil
-      end
-    end)
+    |> Enum.map(fn [game, _] -> get_game_number(game) end)
     |> Enum.sum()
   end
 
@@ -31,13 +20,8 @@ defmodule AdventOfCode.Day02 do
       cubes
     end)
     |> Enum.map(&String.split(&1, ";", trim: true))
-    |> Enum.map(fn tokens -> 
-       Enum.reduce(tokens, {-1, -1, -1}, fn token, {red, green, blue} ->
-        {max(red, count_color(token, "red")), max(green, count_color(token, "green")),
-         max(blue, count_color(token, "blue"))}
-      end)
-    end)
-    |> Enum.map(fn {red, green, blue} -> red*green*blue end)
+    |> Enum.map(&get_min_balls/1)
+    |> Enum.map(fn {red, green, blue} -> red * green * blue end)
     |> Enum.sum()
   end
 
@@ -45,8 +29,28 @@ defmodule AdventOfCode.Day02 do
     case Regex.scan(~r/(\d+) #{color}/, string) do
       [[_, x]] ->
         String.to_integer(x)
+
       [] ->
         -1
     end
+  end
+
+  defp get_game_number(game) do
+    case Regex.run(~r/Game (\d+)/, game) do
+      [_, game_number] -> String.to_integer(game_number)
+      _ -> nil
+    end
+  end
+
+  defp get_min_balls(pulls) do
+    Enum.reduce(pulls, {-1, -1, -1}, fn pull, {red, green, blue} ->
+      {max(red, count_color(pull, "red")), max(green, count_color(pull, "green")),
+       max(blue, count_color(pull, "blue"))}
+    end)
+  end
+
+  defp valid_pull?(pull) do
+        count_color(pull, "red") <= 12 and count_color(pull, "green") <= 13 and
+          count_color(pull, "blue") <= 14
   end
 end
